72b4a82a54cfc008b5dd770af16c41ecb203c722
EdaraAbhinaya <144899102+EdaraAbhinaya@users.noreply.github.com>
2024-04-18 16:27:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+
+
+if __name__ == '__main__':
+    main()
